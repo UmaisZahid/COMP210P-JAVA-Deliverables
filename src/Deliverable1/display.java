913415afd74c8285440dc6509100af007e403c9b
Umais Zahid <umaiszahid@outlook.com>
2018-02-06 15:54:10 +0000

    Added comments, and getter methods in project class.

--- src/Deliverable1/display.java
+++ src/Deliverable1/display.java
@@ -3,11 +3,14 @@ package Deliverable1;
 import java.util.*;
 
 public class display {
+
     // Initialise display/menu related fields.
-    private static Scanner scan = new Scanner(System.in);
-    private static List<Character> menuOptions = Arrays.asList('A', 'a', 'V', 'v', 'C', 'c', 'S', 's', 'Q', 'q','m','M');
+    private static Scanner scan = new Scanner(System.in); // Static scanner instance for input
+    private final static List<Character> menuOptions = Arrays.asList('A', 'a', 'V', 'v', 'C', 'c', 'S', 's', 'Q', 'q','M', 'm'); // List of valid input characters
     public char option; // Variable containing current menu option choice
     public boolean exit = false; // Boolean to determine whether program should exit
+
+    // Strings of paragraphs used in displayAbout() method. Formatted by formatAndPrint method to fit particular number of words per line.
     private static String aboutTextPara1 = "Split-it was designed to help teams allocate credit for a project fairly. " +
             "It works by collecting votes for a project, and allocating marks between team members.";
     private static String aboutTextPara2 = "This idea was originally inspired by the work of Ariel Procaccia and Jonathan Goldman's programmed called " +
@@ -15,6 +18,8 @@ public class display {
             " bills and shared tasks. To run this programme, one needs to create and register a project, collect votes " +
             "from each individual team member and the programme will calculate and display the allocated votes for the project. ";
 
+
+    // Displays menu and requests user to input option.
     public void displayMenu(){
 
         System.out.println("\tWelcome to Split-it");
@@ -25,22 +30,28 @@ public class display {
         System.out.println("\t\tShow Project (S)");
         System.out.println("\t\tQuit (Q)");
         System.out.println();
-        pickOption();
+        pickOption(); // requests user to pick option
 
     }
 
-    private void pickOption(){
-        System.out.print("\tPlease choose an option, or enter m to return to the menu: ");
-        option = scan.next().charAt(0);
 
+    // Requests user to input option, while validating input to ensure it is a valid input character
+    private void pickOption(){
+
+        System.out.print("\tPlease choose an option, or enter m to return to the menu: ");
+        option = scan.next().charAt(0); // Scan first character of input
+
+        // While input is invalid, request for valid input.
         while (!(menuOptions.contains(option))){
             System.out.println();
             System.out.print("\t\tInvalid Input, please enter again: ");
             option = scan.next().charAt(0);
         }
-        System.out.println();
+
+        System.out.println(); // Add spacing
     }
 
+    // Displays about page and requests input
     public void displayAbout(){
         System.out.println();
         System.out.println("\t\t\t\tABOUT: Split-It");
@@ -51,15 +62,16 @@ public class display {
         System.out.println();
         System.out.println();
 
-        pickOption();
+        pickOption(); // Request input option
     }
 
+    // Displays create project page
     public void displayCreateProject(){
         System.out.println("\t\t\t\tCREATE PROJECT\n");
 
         // Request and scan project name
         System.out.print("\tPlease provide a project name: ");
-        scan.next(); // Scan new line character (\n)
+        scan.next(); // Consume new line character (\n)
         String projectName = scan.nextLine();
         System.out.print("\tPlease provide the number of members: ");
         int noMembers = 0;
@@ -76,29 +88,37 @@ public class display {
             }
         }
 
+        // ArrayList of member names.
+        // Of type ArrayList to ensure that it has variable length and more members can be added if need be at a later point.
         ArrayList<String> memberNames = new ArrayList<>();
 
+        // Iterate over number of members, and request members names.
         for(int i = 0; i<noMembers; i++){
             System.out.print("\tPlease provide the name of member " + (i+1) + ": ");
-            scan.next();
+            scan.next(); // Consume new line character ('/n')
             memberNames.add(scan.nextLine());
         }
 
+        // Create project instance, and provide necessary constructor arguments
         project newProject = new project(projectName,noMembers,memberNames);
         System.out.println("\n\t\t\t\tPROJECT CREATED!\n");
-        pickOption();
+        pickOption(); // Request option
     }
 
+    // Display enter votes page. Currently a dummy method for future deliverables, redirects to main menu.
     public void displayEnterVotes(){
         System.out.println("\t\tThis is the enter votes pages");
-        option = 'M';
+        option = 'M'; // Set option to main menu, (redirects automatically)
     }
 
+
+    // Display show projects page. Currently a dummy method for future deliverables, redirects to main menu.
     public void displayShowProjects(){
         System.out.println("\t\tThis is the show project page");
-        option = 'M';
+        option = 'M'; // Set option to main menu, (redirects automatically)
     }
 
+    // Formats and prints an input String so that it has a particular number of words per line.
     private void formatAndPrint(String text, int wordsPerLine){
         String[] textSplit = text.split(" ");
         for (int i=0; i<(textSplit.length); i++){
--- src/Deliverable1/main.java
+++ src/Deliverable1/main.java
@@ -3,17 +3,20 @@ package Deliverable1;
 public class main{
 
     public static void main(String[] args){
+        // Create new display instance.
         display currentDisplay = new display();
-        currentDisplay.displayMenu();
+        currentDisplay.displayMenu(); // Display menu
 
+        // Main execution block: keep programming running until exit boolean is true.
         while(!(currentDisplay.exit)){
+            // Check character of instance variable option and display relevant page.
             switch (currentDisplay.option){
                 case ('A'): case ('a'): currentDisplay.displayAbout(); break;
                 case ('C'): case ('c'): currentDisplay.displayCreateProject(); break;
                 case ('V'): case ('v'): currentDisplay.displayEnterVotes(); break;
                 case ('S'): case ('s'): currentDisplay.displayShowProjects(); break;
                 case ('M'): case ('m'): currentDisplay.displayMenu(); break;
-                case ('Q'): case ('q'): currentDisplay.exit = true; return;
+                case ('Q'): case ('q'): currentDisplay.exit = true; return; // Set exit boolean to true
             }
         }
     }
