de83f6f97ef705574891baf0474696f65a593840
Umais Zahid <umaiszahid@outlook.com>
2018-02-07 00:43:52 +0000

    Cleaner comments

--- src/Deliverable1/main.java
+++ src/Deliverable1/main.java
@@ -3,11 +3,15 @@ package Deliverable1;
 public class main{
 
     public static void main(String[] args){
-        // Create new display instance.
+        //---------------------------------------------------------
+        // Create new display instance, and display menu.
+        //---------------------------------------------------------
         display currentDisplay = new display();
-        currentDisplay.displayMenu(); // Display menu
+        currentDisplay.displayMenu();
 
-        // Main execution block: keep programming running until exit boolean is true.
+        //-------------------------------------------------------------------------
+        // Main execution block: keeps running program until exit boolean is true.
+        //-------------------------------------------------------------------------
         while(!(currentDisplay.exit)){
             // Check character of instance variable option and display relevant page.
             switch (currentDisplay.option){
@@ -16,7 +20,7 @@ public class main{
                 case ('V'): case ('v'): currentDisplay.displayEnterVotes(); break;
                 case ('S'): case ('s'): currentDisplay.displayShowProjects(); break;
                 case ('M'): case ('m'): currentDisplay.displayMenu(); break;
-                case ('Q'): case ('q'): currentDisplay.exit = true; return; // Set exit boolean to true
+                case ('Q'): case ('q'): currentDisplay.exit = true; return; // Set exit boolean to true if input = 'q'
             }
         }
     }
--- src/Deliverable1/project.java
+++ src/Deliverable1/project.java
@@ -4,29 +4,39 @@ import javax.naming.Name;
 import java.util.ArrayList;
 
 public class project {
-    // Initialise fields
+    //---------------------------------------------------------
+    // Initialise project related fields
+    //---------------------------------------------------------
     private String name;
     private int noOfMembers;
     private ArrayList<String> memberNames;
 
-    // Constructor method
+    //---------------------------------------------------------
+    // Constructor:- Create project instance.
+    //---------------------------------------------------------
     public project(String name, int noOfMembers, ArrayList<String> memberNames) {
         this.name = name;
         this.noOfMembers = noOfMembers;
         this.memberNames = memberNames;
     }
 
+    //---------------------------------------------------------
     // Returns the number of members. (getter method)
+    //---------------------------------------------------------
     public int returnNoOfMembers(){
         return this.noOfMembers;
     }
 
+    //---------------------------------------------------------
     // Returns name of project. (getter method)
+    //---------------------------------------------------------
     public String returnName(){
         return this.name;
     }
 
+    //---------------------------------------------------------
     // Returns array list of members names. (getter method)
+    //---------------------------------------------------------
     public ArrayList<String> returnMemberNames(){
         return this.memberNames;
     }
